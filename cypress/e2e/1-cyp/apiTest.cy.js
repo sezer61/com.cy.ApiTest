5a56873cc510cf27a94170e00187ae5a75db8e45
hakankob0@gmail.com <sezer.0734@hotmail.com>
2023-02-01 15:37:05 +0300

    cy

--- cypress/e2e/1-cyp/apiTest.cy.js
+++ cypress/e2e/1-cyp/apiTest.cy.js
@@ -1,10 +1,21 @@
-<reference types="Cypress"/>
+/// <reference types="Cypress"/>
+
 describe('Api Test', () => {
-    it('Headers ', () => {
+    it('Headers -1 ', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/1')
         .its('headers')
         .its('content-type')
         .should('include','application/json; charset=utf-8')
-        
-    });
-});
+
+    })
+        it('Headers -2 ', () => {
+            cy.request({
+                url:'https://pokeapi.co/api/v2/pokemon/1',
+                method:'GET'
+            }).then((response) => {
+                expect(response.headers).to.have.property('content-type').and.to.be.include('application/json; charset=utf-8')
+            
+                
+            })
+        })
+})
