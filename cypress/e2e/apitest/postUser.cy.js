90551524bb8dd352a2539ffa53c6723ec846452b
hakankob0@gmail.com <sezer.0734@hotmail.com>
2023-02-06 13:12:21 +0300

    upd

--- cypress/e2e/apitest/postUser.cy.js
+++ cypress/e2e/apitest/postUser.cy.js
@@ -1,19 +1,35 @@
 /// <reference types="Cypress"/>
 
+const dataJson=require('../../fixtures/createus.json')
+
 describe('post user request',()=>{
     let accessToken='315111cb321d8c90f47968eaab712f5576c81389cd3a7c19acc1e681fb9ab757';
+    let randomText=""
+    let testEmail=""
 
     it('create user test', () => {
+
+        var pattern="ABCDEFGHIJKLMNOPRQRSTUWXYZABCDEFGHİJKLMNOPRSTUWXYZ"
+        for(var i=0; i<10; i++)
+        randomText+=pattern.charAt(Math.floor(Math.random() * pattern.length));
+        testEmail=randomText+"@gmail.com"
+        cy.fixture('createus').then((payload)=>{
+
+        
         cy.request({
+
+
+
+
             method: 'POST',
             url: 'https://gorest.co.in/public/v2/users',
             headers: { 'Authorization':'Bearer ' + accessToken 
             },
             body:{
-                "name": "Test Automations",
-                "gender": "female",
-                "email": "cntaww@gmail.com",
-                "status": "active"
+                "name": dataJson.name,
+                "gender": dataJson.gender,
+                "email": testEmail,
+                "status": dataJson.status
             }
 
 
@@ -21,10 +37,12 @@ describe('post user request',()=>{
 
         }).then((res)=>{
             expect(res.status).to.eq(201)
-            expect(res.body).has.property('email','cntaww@gmail.com')
-            expect(res.body).has.property('name','Test Automations')
-            expect(res.body).has.property('status','active')
-            expect(res.body).has.property('gender','female')
+            expect(res.body).has.property('email',testEmail)
+            expect(res.body).has.property('name',dataJson.name)
+            expect(res.body).has.property('status',dataJson.status)
+            expect(res.body).has.property('gender',dataJson.gender)
         })
     })
+})
+
 })
